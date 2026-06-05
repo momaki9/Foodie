2ba01ab80e2e7365d24e845558823ba8a8575459
momaki9 <mostafa_m9@yahoo.com>
2026-06-04 19:59:50 -0700

    installed cors

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-
+const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -15,8 +15,18 @@ const server = new ApolloServer({
     context: authMiddleware
 });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(
+    cors({
+        origin: [
+            "http://localhost:3000",
+            "https://foodie-orpin-nine.vercel.app"
+        ],
+        credentials: true
+    })
+)
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 const startApolloServer = async () => {
     await server.start();
